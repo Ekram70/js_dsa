deaebf7d2b6453bd18b5130a381f9458f902e89a
Ekram70 <ekramullah70@gmail.com>
2022-09-14 23:29:29 +0600

    doubly linked list added

---
+++ data structures/doublyLinkedList.js
@@ -0,0 +1,88 @@
+// implementing doubly linked list data structure
+
+class Node {
+  constructor(value) {
+    this.value = value;
+    this.next = null;
+    this.prev = null;
+  }
+}
+
+class DoublyLinkedList {
+  constructor(value) {
+    this.head = new Node(value);
+    this.tail = this.head;
+    this.length = 1;
+  }
+
+  append(value) {
+    const newNode = new Node(value);
+    newNode.prev = this.tail;
+    this.tail.next = newNode;
+    this.tail = newNode;
+    this.length++;
+    return this;
+  }
+
+  prepend(value) {
+    const newNode = new Node(value);
+    newNode.next = this.head;
+    this.head.prev = newNode;
+    this.head = newNode;
+    this.length++;
+    return this;
+  }
+
+  insert(index, value) {
+    if (index >= this.length) {
+      return this.append(value);
+    }
+
+    const newNode = new Node(value);
+    const leader = this.traverseToIndex(index - 1);
+    const follower = leader.next;
+    leader.next = newNode;
+    newNode.prev = leader;
+    newNode.next = follower;
+    follower.prev = newNode;
+    this.length++;
+
+    return this;
+  }
+
+  remove(index) {
+    if (index >= 0 && index < this.length) {
+      const unwantedNode = this.traverseToIndex(index);
+      const leader = unwantedNode.prev;
+      const follower = unwantedNode.next;
+
+      leader.next = follower;
+      follower.prev = leader;
+
+      this.length--;
+    }
+    return this;
+  }
+
+  printList() {
+    const array = [];
+    let currentNode = this.head;
+    while (currentNode !== null) {
+      array.push(currentNode.value);
+      currentNode = currentNode.next;
+    }
+    return array;
+  }
+
+  traverseToIndex(index) {
+    if (index >= 0 && index < this.length) {
+      let counter = 0;
+      let currentNode = this.head;
+      while (counter !== index) {
+        currentNode = currentNode.next;
+        counter++;
+      }
+      return currentNode;
+    }
+  }
+}
